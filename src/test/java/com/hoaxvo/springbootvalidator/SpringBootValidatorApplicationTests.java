530d4ea1c0996ad8408beb2ad76c9aa50b9a5791
hoaxvo16 <dev.h1620@gmail.com>
2022-12-17 01:57:24 +0700

    Init project

---
+++ src/test/java/com/hoaxvo/springbootvalidator/SpringBootValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoaxvo.springbootvalidator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootValidatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
